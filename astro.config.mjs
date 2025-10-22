302bea7bc61735fe6c7e14dd38a53881d34254be
WIZARD <100178656+ARACH-Wizard@users.noreply.github.com>
2025-10-22 14:12:09 -0600

    Config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-});
+  site: 'https://arach-wizard.github.io/Concept-Redesign--Sydney-Royal-Botanic-Gardens/',
+})
